dd07e345b1988b164a69cfd410b40189ad3e4e01
Danayit Mulugeta <dayni0603@gmail.com>
2021-07-17 00:27:54 +0300

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'note', loadChildren: () => import('./features/note/note.module') },
+  { path: '', pathMatch: 'full', redirectTo: 'note' },
+  {
+    path: 'note',
+    loadChildren: () =>
+      import('./features/note/note.module').then((m) => m.NoteModule),
+  },
 ];
 
 @NgModule({

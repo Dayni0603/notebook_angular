f5a9e29ab33384471b6552964ea7b52ecfddf750
Danayit Mulugeta <dayni0603@gmail.com>
2021-08-19 20:17:30 +0300

    additional components added

--- src/app/features/note/note-routing.module.ts
+++ src/app/features/note/note-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoteFormComponent } from './components/note-form/note-form.component';
 import { NoteComponent } from './note.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
 
-const routes: Routes = [{ path: '', component: NoteComponent }];
+const routes: Routes = [
+  { path: '', component: NoteListComponent },
+  { path: 'add', component: NoteFormComponent },
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],

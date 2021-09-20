ab47ab0aad87d073b5da5b0d3cde6b399c2e24e9
Danayit Mulugeta <dayni0603@gmail.com>
2021-09-20 19:31:27 +0300

    name fix

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/notebook'),
+      dir: require('path').join(__dirname, './coverage/loanDecision'),
       subdir: '.',
       reporters: [
         { type: 'html' },
